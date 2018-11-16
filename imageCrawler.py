6af1a221ea79754f34680d9578511aa20c6fac57
quan <quan@autointegrity.com.au>
2018-11-16 15:52:30 +0700

    updated code

--- imageCrawler.py
+++ imageCrawler.py
@@ -4,9 +4,10 @@ import os
 import uuid
 from bs4 import BeautifulSoup
 
-def spider(url):
+def spider(max_page):
    page = 1
-   while page <= 1:
+   while page <= max_page:
+        url = 'https://truyenqq.com/truyen-tranh/dao-hai-tac-128-chap-'+ str(page) + '.html' 
         source_code = requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text,"lxml")
@@ -16,12 +17,12 @@ def spider(url):
                 print('find nothing')
                 break
             else:
-                save_image(href)
+                save_image(href, page)
         page += 1
 
-def save_image(url):
+def save_image(url, page):
     file_name = 'one-piece-' + str(uuid.uuid1())
-    forderName = '/home/quan/Pictures/manga/onepice/1'
+    forderName = '/home/quan/Pictures/manga/onepiece/' + str(page)
     pathFileName = os.path.join(forderName)
     if not os.path.exists(pathFileName):
         os.makedirs(pathFileName)
@@ -31,4 +32,4 @@ def save_image(url):
     print( urllib.request.urlretrieve(url, filename=filename))
 
 
-spider('https://truyenqq.com/truyen-tranh/dao-hai-tac-128-chap-1.html')
+spider(10)
